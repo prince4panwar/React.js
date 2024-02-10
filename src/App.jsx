ad426f8e2592a81d5a46af050ca1333abdb3b8f3
Prince Panwar <princepanwar420420@gmail.com>
2024-02-10 22:01:06 +0530

    Components

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Card from "./Card";
+
+function App() {
+  return (
+    <div className="main">
+      {/* <h1>Components</h1> */}
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
