7bf7728282475121793dd275e6a8ce623d8ee350
Prince Panwar <130494609+prince4panwar@users.noreply.github.com>
2024-02-13 21:40:52 +0530

    Add files via upload

---
+++ 4-style-react-components/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Button from "./Button/Button";
+
+function App() {
+  return <Button />;
+}
+
+export default App;
---
+++ 4-style-react-components/src/Button/Button.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import style from "./Button.module.css";
+function Button() {
+  return <button className={style.button}>Click me</button>;
+}
+
+export default Button;
