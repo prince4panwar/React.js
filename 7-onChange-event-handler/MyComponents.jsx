806d6b21cecb9a665873b73635874cff937194c3
Prince Panwar <130494609+prince4panwar@users.noreply.github.com>
2024-02-25 21:14:11 +0530

    onChange event handler

---
+++ 7-onChange-event-handler/MyComponents.jsx
@@ -0,0 +1,74 @@
+//onChange = event handler used primarily with form elements
+//              example <input>, <textarea>, <select>, <radio>
+//              Triggers a function every time the value of the input changes
+
+import React, { useState } from "react";
+
+function MyComponents() {
+  const [name, setName] = useState("Guest");
+  const [quantity, setQuantity] = useState(1);
+  const [comment, setComment] = useState("");
+  const [payment, setPayment] = useState("");
+  const [shipping, setShipping] = useState("Delivery");
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
+  function handleQuantityChange(e) {
+    setQuantity(e.target.value);
+  }
+  function handleCommentChange(e) {
+    setComment(e.target.value);
+  }
+  function handlePaymentChange(e) {
+    setPayment(e.target.value);
+  }
+  function handleShippingChange(e) {
+    setShipping(e.target.value);
+  }
+  return (
+    <div>
+      <input value={name} onChange={handleNameChange} />
+      <p>Name: {name}</p>
+      <input value={quantity} onChange={handleQuantityChange} type="number" />
+      <p>Quantity: {quantity}</p>
+      <textarea
+        value={comment}
+        onChange={handleCommentChange}
+        placeholder="Enter delivery instruction"
+      />
+      <p>Comment: {comment}</p>
+
+      <select value={payment} onChange={handlePaymentChange}>
+        <option value="Select an option">Select an option</option>
+        <option value="Visa">Visa</option>
+        <option value="Mastercard">Mastercard</option>
+        <option value="UPI">UPI</option>
+      </select>
+      <p>Payment: {payment}</p>
+
+      <label>
+        <input
+          type="radio"
+          value="Pick Up"
+          checked={shipping === "Pick Up"}
+          onChange={handleShippingChange}
+        />
+        Pick Up
+      </label>
+      <br />
+      <label>
+        <input
+          type="radio"
+          value="Delivery"
+          checked={shipping === "Delivery"}
+          onChange={handleShippingChange}
+        />
+        Delivery
+      </label>
+      <p>Shipping: {shipping}</p>
+    </div>
+  );
+}
+
+export default MyComponents;
---
+++ 7-onChange-event-handler/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MyComponents from "../MyComponents";
+
+function App() {
+  return <MyComponents />;
+}
+
+export default App;
