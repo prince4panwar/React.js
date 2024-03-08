6c2abeeb19bf8ff8d2d35d342ca404033fdd3ad6
Prince Panwar <130494609+prince4panwar@users.noreply.github.com>
2024-03-08 17:46:18 +0530

    Add files via upload

---
+++ 15-Digital-Clock/public/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import DigitalClock from "./DigitalClock";
+
+function App() {
+  return (
+    <>
+      <DigitalClock />
+      <DigitalClock />
+      <DigitalClock />
+      <DigitalClock />
+      <DigitalClock />
+    </>
+  );
+}
+
+export default App;
